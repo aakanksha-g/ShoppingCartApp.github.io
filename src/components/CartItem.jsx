f51f7b0559224ce203bdcad417c7a14419d183e7
aakanksha-g <78199633+aakanksha-g@users.noreply.github.com>
2023-12-07 01:23:17 +0530

    Files Upload

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,44 @@
+
+import {FcDeleteDatabase} from "react-icons/fc"
+import { useDispatch } from "react-redux";
+import { remove } from "../redux/Slices/CartSlice";
+import { toast } from "react-hot-toast";
+
+const CartItem = ({item, itemIndex}) => {
+  const dispatch = useDispatch();
+
+  const removeFromCart = () => {
+    dispatch(remove(item.id));
+    toast.success("Item Removed");
+  }
+
+  return (
+    <div className="flex flex-row items-center justify-between 
+    hover:scale-110 transition duration-300 ease-in gap-3 p-4 mt-10 ml-5 rounded-xl outline">
+
+      <div>
+
+        <div className="h-[180px]">
+          <img src={item.image} className="h-full w-full " />
+        </div>
+        <div>
+          <h1 className="text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1">{item.title}</h1>
+          <h1 className="w-40 text-gray-400 font-normal text-[10px] text-left">{item.description}</h1>
+          <div>
+            <p className="text-green-600 font-semibold">${item.price}</p>
+            <div
+            onClick={removeFromCart}>
+              <FcDeleteDatabase/>
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
+
+    </div>
+  );
+};
+
+export default CartItem;
