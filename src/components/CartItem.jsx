fdc6cca805605a0691f0c462723b335c6622d387
aakanksha-g <78199633+aakanksha-g@users.noreply.github.com>
2023-12-07 01:55:28 +0530

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,4 @@
-
-import {FcDeleteDatabase} from "react-icons/fc"
+import {RiDeleteBin7Fill} from "react-icons/ri"
 import { useDispatch } from "react-redux";
 import { remove } from "../redux/Slices/CartSlice";
 import { toast } from "react-hot-toast";
@@ -13,31 +12,25 @@ const CartItem = ({item, itemIndex}) => {
   }
 
   return (
-    <div className="flex flex-row items-center justify-between 
-    hover:scale-110 transition duration-300 ease-in gap-3 p-4 mt-10 ml-5 rounded-xl outline">
-
-      <div>
-
-        <div className="h-[180px]">
-          <img src={item.image} className="h-full w-full " />
+    <>
+    <div className="flex items-center p-5 justify-between bg-violet-200 mt-2 mb-2 rounded-xl">
+      <div className="flex p-3">
+        <img src={item.image} className="h-28 rounded-lg" alt="product" />
+        <div className="ml-10 self-start space-y-5">
+          <h1 className="text-xl text-purple-700 font-semibold">
+            {item.title}
+          </h1>
+          <p>${item.price}</p>
         </div>
-        <div>
-          <h1 className="text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1">{item.title}</h1>
-          <h1 className="w-40 text-gray-400 font-normal text-[10px] text-left">{item.description}</h1>
-          <div>
-            <p className="text-green-600 font-semibold">${item.price}</p>
-            <div
-            onClick={removeFromCart}>
-              <FcDeleteDatabase/>
-            </div>
-          </div>
-
-        </div>
-
-
       </div>
-
+      <div
+        onClick={removeFromCart}
+        className="bg-purple-300 hover:bg-purple-400 transition-transform duration-300 cursor-pointer rounded-full p-3 mr-3"
+      >
+        < RiDeleteBin7Fill className="text-gray-800" />
+      </div>
     </div>
+  </>
   );
 };
 
